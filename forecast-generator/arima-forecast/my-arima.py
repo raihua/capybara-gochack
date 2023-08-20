f8b58f339f1c5cd6d27c8f2e6821a4f9de0ebd71
raihua <raihua.dev@gmail.com>
2023-08-20 12:47:37 +0930

    Refactor: switched to new arima model

--- forecast-generator/arima-forecast/my-arima.py
+++ forecast-generator/arima-forecast/my-arima.py
@@ -3,51 +3,48 @@ import pandas as pd
 from matplotlib import pyplot as plt
 from statsmodels.tsa.arima.model import ARIMA
 
-
 def load_data(file_path):
     series = read_csv(file_path, header=0, parse_dates=[0], index_col=0, date_format='%Y')
     return series
 
-
-def train_arima_model(data, order=(4, 1, 1)):
+def train_arima_model(data, order=(25, 1, 5)):
     model = ARIMA(data, order=order)
     model_fit = model.fit()
     return model_fit
-
 
 def generate_forecast(model_fit, start_date, end_date):
     forecast_years = pd.date_range(start=start_date, end=end_date, freq='AS-JAN')
     forecast = model_fit.forecast(steps=len(forecast_years))
     return forecast
 
-
 def plot_forecast(data, forecast_years, forecast):
     plt.plot(data.index, data.values, label='Historical Data')
     plt.plot(forecast_years, forecast, color='red', label='Forecast')
     plt.xlabel('Time')
-    plt.ylabel('EVSales')
+    plt.ylabel('GDP percent change')
     plt.legend()
     plt.show()
 
-
 def main():
-    file_path = 'ev-sales.csv'
+    file_path = 'gdp-year.csv'
     start_date = '2023-01-01'
     end_date = '2050-01-01'
     
     data = load_data(file_path)
-    model_fit = train_arima_model(data)
+    model_fit = train_arima_model(data['GDP growth (annual %)'])
     forecast = generate_forecast(model_fit, start_date, end_date)
     forecast_years = pd.date_range(start=start_date, end=end_date, freq='AS-JAN')
     
-    # Extend the existing data with forecasted values
-    extended_data = pd.concat([data, pd.Series(forecast, index=forecast_years)])
+    # Create a new DataFrame with forecasted values and switch the columns
+    forecast_data = pd.DataFrame({'Year': forecast_years, 'GDP growth (annual %)': forecast})
     
-    # Export the extended data to a CSV file
-    extended_data.to_csv('extended_ev_sales.csv')
+    # Combine the original data and forecast data
+    combined_data = pd.concat([data, forecast_data.set_index('Year')]).sort_index()
     
-    plot_forecast(data, forecast_years, forecast)
+    # Export the combined data to a CSV file
+    combined_data.to_csv('extended_gdp.csv')
 
+    plot_forecast(data, forecast_years, forecast)
 
 if __name__ == "__main__":
     main()
